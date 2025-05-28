ff5937d5668c2d3f9546069f314591e441e4aac0
Jetsada Wijit <jetsadawijit@outlook.com>
2025-05-28 19:03:10 +0000

    Add json

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(cors({
 
 // Middleware
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(
